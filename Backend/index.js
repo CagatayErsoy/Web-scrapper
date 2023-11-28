f4b288fdfe5e6416225a4ce79ea4f9bc65b5e70c
çağatay ersoy <cagatayersoy@cagatays-MacBook-Pro.local>
2023-11-27 16:28:15 -0800

    trying to fix render issues

--- Backend/index.js
+++ Backend/index.js
@@ -65,6 +65,6 @@ app.post('/scrape', async (req, res) => {
 
 
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(port,'0.0.0.0', () => {
+    console.log(`Server is running on ${port}`);
 });
